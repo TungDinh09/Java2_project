043a7ad25571ab7433ae869cc7b2ca0bb4fdac51
c2202lm <c2202lm.pmquan@aptech.vn>
2023-07-12 01:28:45 +0700

    view layout in main

---
+++ src/java2_project/Main.java
@@ -0,0 +1,18 @@
+package java2_project;
+
+import View.HomePage;
+import View.Register;
+import View.Sign_in;
+
+public class Main {
+    public static void main(String[] args) {
+        HomePage homePage = new HomePage();
+        homePage.setVisible(true);
+        Register register = new Register();
+        register.setVisible(true);
+        register.setLocationRelativeTo(null);
+        Sign_in signIn = new Sign_in();
+        signIn.setVisible(true);
+        signIn.setLocationRelativeTo(null);
+    }
+}
