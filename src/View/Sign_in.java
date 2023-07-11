033881ccb7596bf9ad2fe047716c5d3f61ecdf41
Tung Dinh <tungdinh21092003@gmail.com>
2023-07-11 20:27:38 +0700

    tung

--- src/View/Sign_in.java
+++ src/View/Sign_in.java
@@ -181,7 +181,9 @@ public class Sign_in extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        Register jf1 = new Register();
+        jf1.show();
+        dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
